becd8265efe0d2fecb72f8d9dab8d4c2cb509759
DEIMERR <deimer090@gmail.com>
2021-10-07 19:24:05 -0500

    Agrego la nueva conexión y una página de html

--- src/main/java/com/planck/DAO/Connection.java
+++ src/main/java/com/planck/DAO/Connection.java
@@ -3,17 +3,19 @@ package com.planck.DAO;
 import java.sql.*;
 
 public class Connection {
-    String database = "store";
+    String database = "tiendagrupo60";
     String user = "root";
-    String password = "admin";
-    String url = "jdbc:mysql://localhost/" + database;
+    String password = "admin123";
+    String port = "3306";
+    String url = "tiendagrupo60.c47knbsonjdi.us-east-2.rds.amazonaws.com";
+    String jdbcUrl = "jdbc:mysql://" + url+ ":" + port + "/" + database + "?user=" + user + "&password=" + password;
 
     java.sql.Connection connection = null;
 
     public java.sql.Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(url, user, password);
+            connection = DriverManager.getConnection(jdbcUrl);
             if (connection != null) {
                 System.out.println("Conexión exitosa a la base de datos");
             }
